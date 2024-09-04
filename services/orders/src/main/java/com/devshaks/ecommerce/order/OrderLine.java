0fa981683948098053f784a7f8703e119669fb40
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2024-09-04 20:45:01 +0100

    order entity

---
+++ services/orders/src/main/java/com/devshaks/ecommerce/order/OrderLine.java
@@ -0,0 +1,25 @@
+package com.devshaks.ecommerce.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+    private double quantity;
+}
